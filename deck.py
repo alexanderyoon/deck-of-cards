c7ca5416c643a537b89984422baf00c22fd856d5
Alexander Yoon <alexanderyoon@Michaels-MacBook-Air.local>
2018-10-28 22:23:17 -0600

    Added some comments. Fixed the blank lines that we printed for spacing. In shuffle_deck, we assign a random number to the 2nd index (j), instead of setting it to the 1st index (i).

--- deck.py
+++ deck.py
@@ -14,33 +14,38 @@ def create_deck():
     for n in range(2, 11):
         ranks.append(str(n))
 
-    ranks = ranks + ['jack', 'queen', 'king', 'ace']
+    ranks += ['jack', 'queen', 'king', 'ace']
+    # Same as: ranks = ranks + ['jack', 'queen', 'king', 'ace']
 
     # Create a full deck of one card of each rank for each suit.
     deck = []
     for s in suits:
         for r in ranks:
+            # TODO: add a 'value' field for each card that is an integer from 2 to 13.
             card = {'suit': s, 'rank': r}
             deck.append(card)
-            
+
     return deck
 
 def shuffle_deck(deck, hands=None, shuffle_count=200):
     """Shuffle the deck.
 
     """
+    # Shortcut for: if hands is not None:
     if hands:
         # Add hands back into the deck.
         for hand in hands:
             deck += hand
         
+    # TODO: shuffle more realistically: split the deck near the middle, then
+    # interleave the two parts together back into one deck.
     for i in range(shuffle_count):
         # Pick a random index.
         i = random.randint(0, len(deck) - 1)
         
         # Pick a 2nd random index, and make sure that it is different
         # from the 1st.
-        j = i
+        j = random.randint(0, len(deck) - 1)
         while j == i:
             j = random.randint(0, len(deck) - 1)
 
@@ -51,6 +56,7 @@ def deal_deck(deck, hand_count, cards_per_hand=None):
     """Deal the deck into the specified number of hands (minimum of 2).
 
     """
+    #assert(deck is list)
     assert(hand_count >= 2)
     hands = []
 
@@ -59,7 +65,7 @@ def deal_deck(deck, hand_count, cards_per_hand=None):
         hand = []
         hands.append(hand)
 
-    if cards_per_hand:
+    if cards_per_hand: # same as: if cards_per_hand is not None
         cards_to_deal = hand_count * cards_per_hand
     else:
         cards_to_deal = len(deck)
@@ -80,43 +86,45 @@ def deal_deck(deck, hand_count, cards_per_hand=None):
 
     return hands
 
+print('__name__ = ' + __name__)
+
 if __name__ == '__main__':
     print('Here is a deck:')
     deck = create_deck()
     pprint(deck)
-    print()
+    print('')
 
     print('Let\'s shuffle the deck:')
     shuffle_deck(deck)
     pprint(deck)
-    print()
+    print('')
 
     print('Let\'s shuffle the deck again:')
     shuffle_deck(deck)
     pprint(deck)
-    print()
+    print('')
 
     hand_count = 2
     print('Let\'s deal {} hands:'.format(hand_count))
-    print()
+    print('')
     hands = deal_deck(deck, hand_count)
     for i, hand in enumerate(hands):
         print('Here is hand #{}:'.format(i))
         pprint(hand)
-        print()
+        print('')
 
     shuffle_deck(deck, hands)
     
-    hand_count = 2
+    hand_count = 4
     cards_per_hand = 5
     print('Let\'s deal {} hands, {} cards each:'.format(
         hand_count, cards_per_hand))
-    print()
+    print('')
     hands = deal_deck(deck, hand_count, cards_per_hand)
     for i, hand in enumerate(hands):
         print('Here is hand #{}:'.format(i))
         pprint(hand)
-        print()
+        print('')
     print('Here is the rest of the deck ({} cards left):'.format(len(deck)))
     pprint(deck)
-    print()
+    print('')
