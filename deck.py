103864cb472c64d6f4fb16bdeb325ece82b4fcf2
Michael Yoon <michael@yoon.org>
2018-10-28 19:30:54 -0600

    Add a function to deal the deck into hands. Added optional hands argument to shuffle_deck.

--- deck.py
+++ deck.py
@@ -4,6 +4,7 @@ from pprint import pprint
 def create_deck():
     """Create a list of 52 cards, where each card is a dictionary with 2
     fields: 'suit' and 'rank'.
+
     """
     # Create all the suits.
     suits = ['heart', 'diamond', 'club', 'spade']
@@ -24,10 +25,15 @@ def create_deck():
             
     return deck
 
-def shuffle_deck(deck, shuffle_count=200):
+def shuffle_deck(deck, hands=None, shuffle_count=200):
     """Shuffle the deck.
 
     """
+    if hands:
+        # Add hands back into the deck.
+        for hand in hands:
+            deck += hand
+        
     for i in range(shuffle_count):
         # Pick a random index.
         i = random.randint(0, len(deck) - 1)
@@ -41,7 +47,39 @@ def shuffle_deck(deck, shuffle_count=200):
         # Swap the 2 cards.
         deck[i], deck[j] = deck[j], deck[i]
 
+def deal_deck(deck, hand_count, cards_per_hand=None):
+    """Deal the deck into the specified number of hands (minimum of 2).
+
+    """
+    assert(hand_count >= 2)
+    hands = []
+
+    # Create the specified number of hands as empty lists.
+    for i in range(hand_count):
+        hand = []
+        hands.append(hand)
+
+    if cards_per_hand:
+        cards_to_deal = hand_count * cards_per_hand
+    else:
+        cards_to_deal = len(deck)
+    print('{} cards to deal.'.format(cards_to_deal))
+    
+    i = 0
+    while cards_to_deal > 0:
+        card = deck.pop()
+        hand = hands[i]
+        hand.append(card)
         
+        # Deal the next card to the next hand.
+        i += 1
+        if i == hand_count:
+            i = 0
+
+        cards_to_deal -= 1
+
+    return hands
+
 if __name__ == '__main__':
     print('Here is a deck:')
     deck = create_deck()
@@ -55,5 +93,30 @@ if __name__ == '__main__':
 
     print('Let\'s shuffle the deck again:')
     shuffle_deck(deck)
+    pprint(deck)
+    print()
+
+    hand_count = 2
+    print('Let\'s deal {} hands:'.format(hand_count))
+    print()
+    hands = deal_deck(deck, hand_count)
+    for i, hand in enumerate(hands):
+        print('Here is hand #{}:'.format(i))
+        pprint(hand)
+        print()
+
+    shuffle_deck(deck, hands)
+    
+    hand_count = 2
+    cards_per_hand = 5
+    print('Let\'s deal {} hands, {} cards each:'.format(
+        hand_count, cards_per_hand))
+    print()
+    hands = deal_deck(deck, hand_count, cards_per_hand)
+    for i, hand in enumerate(hands):
+        print('Here is hand #{}:'.format(i))
+        pprint(hand)
+        print()
+    print('Here is the rest of the deck ({} cards left):'.format(len(deck)))
     pprint(deck)
     print()
