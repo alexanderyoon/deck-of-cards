60de80697599b780f8e8a3c58568af02f8ab2345
Michael Yoon <michael@yoon.org>
2018-10-28 18:21:45 -0600

    Moved deck creating and shuffling to functions.

--- deck.py
+++ deck.py
@@ -1,40 +1,59 @@
 import random
 from pprint import pprint
 
-def shuffle_deck(deck):
-    # Shuffle the deck.
-    shuffled_deck = []
-    
-    print('Shuffling the deck...')
-    while len(deck) > 0:
-        index = random.randint(0, len(deck) - 1)
-        print('Picking card #{}'.format(index))
-        card = deck.pop(index)
-        shuffled_deck.append(card)
+def create_deck():
+    """Create a list of 52 cards, where each card is a dictionary with 2
+    fields: 'suit' and 'rank'.
+    """
+    # Create all the suits.
+    suits = ['heart', 'diamond', 'club', 'spade']
+
+    # Create all the ranks, numbers first, then face cards.
+    ranks = []
+    for n in range(2, 11):
+        ranks.append(str(n))
+
+    ranks = ranks + ['jack', 'queen', 'king', 'ace']
+
+    # Create a full deck of one card of each rank for each suit.
+    deck = []
+    for s in suits:
+        for r in ranks:
+            card = {'suit': s, 'rank': r}
+            deck.append(card)
+            
+    return deck
+
+def shuffle_deck(deck, shuffle_count=200):
+    """Shuffle the deck.
+
+    """
+    for i in range(shuffle_count):
+        # Pick a random index.
+        i = random.randint(0, len(deck) - 1)
         
-    return shuffled_deck
+        # Pick a 2nd random index, and make sure that it is different
+        # from the 1st.
+        j = i
+        while j == i:
+            j = random.randint(0, len(deck) - 1)
 
-# Create all the suits.
-suits = ['heart', 'diamond', 'club', 'spade']
+        # Swap the 2 cards.
+        deck[i], deck[j] = deck[j], deck[i]
 
-# Create all the ranks, numbers first, then face cards.
-ranks = []
-for n in range(2, 11):
-    ranks.append(str(n))
+        
+if __name__ == '__main__':
+    print('Here is a deck:')
+    deck = create_deck()
+    pprint(deck)
+    print()
 
-ranks = ranks + ['jack', 'queen', 'king', 'ace']
+    print('Let\'s shuffle the deck:')
+    shuffle_deck(deck)
+    pprint(deck)
+    print()
 
-# Create a full deck of one card of each rank for each suit.
-deck = []
-for s in suits:
-    for r in ranks:
-        card = {'suit': s, 'rank': r}
-        deck.append(card)
-
-print('Here is the deck:')
-pprint(deck)
-print()
-
-print()
-print('Here is the shuffled deck:')
-pprint(shuffle_deck(deck))
+    print('Let\'s shuffle the deck again:')
+    shuffle_deck(deck)
+    pprint(deck)
+    print()
