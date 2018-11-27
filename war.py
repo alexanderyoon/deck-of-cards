442fb53c72cfbb8dbf3bb6e2b9e66cf2f318153e
Alexander Yoon <alexander@yoon.org>
2018-11-27 13:06:00 -0700

    Fixed mistake that ended the game after one round

--- war.py
+++ war.py
@@ -93,7 +93,7 @@ while len(user_hand) > 0 and len(computer_hand) > 0:
 				computer_hand.append(computer_knight)
 				print('Your deck is {} cards long'.format(len(user_hand)))
 				print('The computer\'s deck is {} cards long'.format(len(computer_hand)))
-	elif cmd == 'q'
+	elif cmd == 'q':
 		print('Goodbye.')
 		break
 	else:
