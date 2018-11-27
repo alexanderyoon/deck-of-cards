bb01b8e0090a91846b9ea5c38976eca7f20f4a06
Alexander Yoon <alexander@yoon.org>
2018-11-27 14:28:42 -0700

    Finished war loop and removed a problem that caused the game to end if you entered a unknown command

--- war.py
+++ war.py
@@ -12,6 +12,53 @@ if __name__ == '__main__':
 	user_hand = hands[0]
 	computer_hand = hands[1]
 
+def war():
+	print("WAR!")	
+	user_war1 = user_hand.pop()
+	user_war2 = user_hand.pop()
+	user_war3 = user_hand.pop()
+	user_knight = user_hand.pop()
+	computer_war1 = computer_hand.pop()
+	computer_war2 = computer_hand.pop()
+	computer_war3 = computer_hand.pop()
+	computer_knight = computer_hand.pop()																	
+	if user_knight['value'] > computer_knight['value']: 
+		print('Your Knight: {}'.format(user_knight))
+		print('The Enemy\'s Knight: {}'.format(computer_knight))
+		print('You win!')
+		print('Your cards: {}, {}, {}'.format(user_war1, user_war2, user_war3))
+		print('The Enemy\'s cards: {}, {}, {}'.format(computer_war1, computer_war2, computer_war3))
+		user_hand.append(user_war1)
+		user_hand.append(user_war2)
+		user_hand.append(user_war3)
+		user_hand.append(user_knight)
+		user_hand.append(computer_war1)
+		user_hand.append(computer_war2)
+		user_hand.append(computer_war3)
+		user_hand.append(computer_knight)
+		print('Your deck is {} cards long'.format(len(user_hand)))
+		print('The computer\'s deck is {} cards long'.format(len(computer_hand)))
+	elif user_knight['value'] < computer_knight['value']:
+		print('Your Knight: {}'.format(user_knight))
+		print('The Enemy\'s Knight: {}'.format(computer_knight))
+		print('You Lose')
+		print('Your cards: {}, {}, {}'.format(user_war1, user_war2, user_war3))
+		print('The Enemy\'s cards: {}, {}, {}'.format(computer_war1, computer_war2, computer_war3))
+		computer_hand.append(user_war1)
+		computer_hand.append(user_war2)
+		computer_hand.append(user_war3)
+		computer_hand.append(user_knight)
+		computer_hand.append(computer_war1)
+		computer_hand.append(computer_war2)
+		computer_hand.append(computer_war3)
+		computer_hand.append(computer_knight)
+		print('Your deck is {} cards long'.format(len(user_hand)))
+		print('The computer\'s deck is {} cards long'.format(len(computer_hand)))
+	else:
+		print('Your Knight: {}'.format(user_knight))
+		print('The Enemy\'s Knight: {}'.format(computer_knight))
+		war()
+
 while len(user_hand) > 0 and len(computer_hand) > 0:
 	cmd = input('Enter "p" to play a card or "q" to quit: ')
 	if cmd == 'p':
@@ -36,65 +83,20 @@ while len(user_hand) > 0 and len(computer_hand) > 0:
 		else:
 			print('You: {}'.format(user_card))
 			print('The Enemy: {}'.format(computer_card))
-			print('<========|==)WAR(==|========>')
-			user_war1 = user_hand.pop()
-			user_war2 = user_hand.pop()
-			user_war3 = user_hand.pop()
-			user_knight = user_hand.pop()
-			computer_war1 = computer_hand.pop()
-			computer_war2 = computer_hand.pop()
-			computer_war3 = computer_hand.pop()
-			computer_knight = computer_hand.pop()
-			if user_knight['value'] > computer_knight['value']: 
-				print('Your Knight: {}'.format(user_knight))
-				print('The Enemy\'s Knight: {}'.format(computer_knight))
-				print('You win!')
-				print('Your cards: {}, {}, {}'.format(user_war1, user_war2, user_war3))
-				print('The Enemy\'s cards: {}, {}, {}'.format(computer_war1, computer_war2, computer_war3))
-				user_hand.append(user_war1)
-				user_hand.append(user_war2)
-				user_hand.append(user_war3)
-				user_hand.append(user_knight)
-				user_hand.append(computer_war1)
-				user_hand.append(computer_war2)
-				user_hand.append(computer_war3)
-				user_hand.append(computer_knight)
-				print('Your deck is {} cards long'.format(len(user_hand)))
-				print('The computer\'s deck is {} cards long'.format(len(computer_hand)))
-			elif user_knight['value'] < computer_knight['value']:
-				print('Your Knight: {}'.format(user_knight))
-				print('The Enemy\'s Knight: {}'.format(computer_knight))
-				print('You Lose')
-				print('Your cards: {}, {}, {}'.format(user_war1, user_war2, user_war3))
-				print('The Enemy\'s cards: {}, {}, {}'.format(computer_war1, computer_war2, computer_war3))
-				computer_hand.append(user_war1)
-				computer_hand.append(user_war2)
-				computer_hand.append(user_war3)
-				computer_hand.append(user_knight)
-				computer_hand.append(computer_war1)
-				computer_hand.append(computer_war2)
-				computer_hand.append(computer_war3)
-				computer_hand.append(computer_knight)
-				print('Your deck is {} cards long'.format(len(user_hand)))
-				print('The computer\'s deck is {} cards long'.format(len(computer_hand)))
-			else:
-				print('Your Knight: {}'.format(user_knight))
-				print('The Enemy\'s Knight: {}'.format(computer_knight))
-				print('This is too much war!')
-				print('Your cards: {}, {}, {}'.format(user_war1, user_war2, user_war3))
-				print('The Enemy\'s cards: {}, {}, {}'.format(computer_war1, computer_war2, computer_war3))
-				user_hand.append(user_war1)
-				user_hand.append(user_war2)
-				user_hand.append(user_war3)
-				user_hand.append(user_knight)
-				computer_hand.append(computer_war1)
-				computer_hand.append(computer_war2)
-				computer_hand.append(computer_war3)
-				computer_hand.append(computer_knight)
-				print('Your deck is {} cards long'.format(len(user_hand)))
-				print('The computer\'s deck is {} cards long'.format(len(computer_hand)))
+			war()
 	elif cmd == 'q':
 		print('Goodbye.')
 		break
+	elif cmd == 'Peace':
+		print("																																																																					")
+		break
 	else:
-		print('Sorry, didn\'t recognize your command: {}'.format(s))
+		print('Sorry, didn\'t recognize your command: {}'.format(cmd))
+
+if len(user_hand) == 0:
+	print('You have lost the game.')
+elif len(computer_hand) == 0:
+	print('You win!') 
+elif cmd == 'Peace':
+	print('You win!')
+
